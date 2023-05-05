7b559f846925009a828f3dca53583512ab932260
aolufisayo <phissycodes@hotmail.com>
2023-05-05 13:36:59 +0100

    changed code for hosting on vercel

--- GreatKart/settings/prod.py
+++ GreatKart/settings/prod.py
@@ -12,39 +12,4 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['greatcart-ecommerce-shop.herokuapp.com']
-
-# Heroku
-DEBUG_PROPAGATE_EXCEPTIONS = True
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
-            'datefmt': '%d/%b/%Y %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler'
-        },
-    },
-    'loggers': {
-        'MYAPP': {
-            'handlers': ['console'],
-            'level': 'DEBUG'
-        },
-    },
-
-
-}
-
-# Heroku settings
-django_on_heroku.settings(locals(), staticfiles=False)
-#del DATABASES['default']['OPTIONS']['sslmode']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
