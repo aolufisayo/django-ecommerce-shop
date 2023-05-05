840a0992d1f7f942b2bc0d9dd8ace053248e0e2a
aolufisayo <phissycodes@hotmail.com>
2023-05-05 11:54:18 +0100

    changed ststicfile dir

--- GreatKart/settings/base.py
+++ GreatKart/settings/base.py
@@ -129,7 +129,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    'GreatKart/static',
+    # 'GreatKart/static',
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
