36772cdd0b6e5fe5bc2fdacc0354dcba2198080c
aolufisayo <phissycodes@hotmail.com>
2023-05-05 14:25:30 +0100

    refactor settings file

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import dj_database_url
-from dotenv import load_dotenv, find_dotenv
+
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Application definition
@@ -77,20 +76,13 @@ AUTH_USER_MODEL = "account.Account"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-load_dotenv(find_dotenv())
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
-}
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- GreatKart/wsgi.py
+++ GreatKart/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GreatKart.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GreatKart.settings')
 
 application = get_wsgi_application()
 app = application
