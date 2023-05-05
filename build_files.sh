73d49b07d0c40d4b619d24b49475dda0058b581b
aolufisayo <phissycodes@hotmail.com>
2023-05-05 16:08:51 +0100

    changed build files

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput --clear
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
