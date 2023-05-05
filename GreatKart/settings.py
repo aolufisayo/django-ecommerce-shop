c1adb55e489c749fd8a0bdcac41f9a670c867e2f
aolufisayo <phissycodes@hotmail.com>
2023-05-05 15:35:58 +0100

    comment sqlite db usage

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -89,12 +89,12 @@ ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
