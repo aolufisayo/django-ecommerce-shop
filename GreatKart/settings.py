87ea32503057b16ee1cc53156493d25005b320c3
aolufisayo <phissycodes@hotmail.com>
2023-05-05 15:58:40 +0100

    changed default database to postgresql

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
@@ -90,11 +90,13 @@ ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo "BUILD START"
 pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput --clear
 echo "BUILD END"
