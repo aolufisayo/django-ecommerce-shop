9af6921390962b557435411f38ee7a0bd472bdc4
aolufisayo <phissycodes@hotmail.com>
2023-05-05 16:28:28 +0100

    modify config files

--- GreatKart/settings.py
+++ GreatKart/settings.py
@@ -83,7 +83,7 @@ SECRET_KEY = "ajjsjjdujjkksk"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
 
 
 # Database
--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 echo "BUILD START"
-pip install -r requirements.txt
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
